89a7c7fc898e37a90a094ebb1b682b3de46a069b
Leonardo Amato <leo.amato@hotmail.com>
2022-10-31 17:28:07 +0100

    firestore flutter

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:auth/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.blue,
+        ),
+        home: MyData());
+  }
+}
+
+class MyData extends StatefulWidget {
+  const MyData({super.key});
+
+  @override
+  State<MyData> createState() => _MyDataState();
+}
+
+class _MyDataState extends State<MyData> {
+  @override
+  Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
+    CollectionReference users = FirebaseFirestore.instance.collection('musica');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Futter Demo'),
+      ),
+      body: Center(child: UserInformation()),
+    );
+  }
+}
+
+class UserInformation extends StatefulWidget {
+  @override
+  _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('musica').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['bandas']),
+              subtitle: Text(data['discos']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
